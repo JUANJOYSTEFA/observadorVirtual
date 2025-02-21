bd8952f9e6c3024abd12100e5a7fbe282f8e2b2b
juanjoystefa <juanjoseromerogomez0@gmail.com>
2025-02-21 13:38:45 -0500

    Agrego la carpeta Modulos

---
+++ modulos/Observador/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from modulos.Observador.models import *
+
+admin.site.register(Grado)
+admin.site.register(Estudiante)
+admin.site.register(Acudiente)
+admin.site.register(Administrativos)
+admin.site.register(Faltas)
+admin.site.register(Observacion)
+admin.site.register(Citaciones)
