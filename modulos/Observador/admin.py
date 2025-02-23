fb2e0b4646084f68c5a6f4783849cd1595b2bf0e
Juan José <juanjoseromerogomez0@gmail.com>
2025-02-22 23:54:22 -0500

    Arreglo los errores que tenía y empiezo a crear los textos para el admin.py

--- modulos/Observador/admin.py
+++ modulos/Observador/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from modulos.Observador.models import *
 
-admin.site.register(Grados)
-admin.site.register(Estudiantes)
-admin.site.register(Acudientes)
+admin.site.register(Grado)
+admin.site.register(Estudiante)
+admin.site.register(Acudiente)
 admin.site.register(Administrativos)
 admin.site.register(Faltas)
-admin.site.register(Observaciones)
+admin.site.register(Observacion)
 admin.site.register(Citaciones)
