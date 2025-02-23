96a711cc58b4788dbe217d9d46d88279eedef4b3
Juan José <juanjoseromerogomez0@gmail.com>
2025-02-22 23:54:22 -0500

    Arreglo los errores que tenía y empiezo a crear los textos para el admin.py

--- modulos/Observador/models.py
+++ modulos/Observador/models.py
@@ -1,41 +1,57 @@
 from django.db import models
 
-class Grados(models.Model):
+class Grado(models.Model):
     idGrado = models.AutoField(primary_key=True)
-    grado = models.CharField(max_length=3)
+    grado = models.CharField(max_length=10, null=True, blank=True)
     ciclo = models.CharField(max_length=7)
 
     def __str__(self):
-        return "Id: {0}, Grado: {1}, Ciclo: {2}".format(self.idGrado, self.grado, self.ciclo)
-    
-    class Meta:
-        db_table = 'Grados'
+        return f"Ciclo: {self.ciclo}, Grado: {self.grado}"
 
-class Estudiantes(models.Model):
+from django.db import models
+
+class Estudiante(models.Model):
     idEstudiante = models.AutoField(primary_key=True)
+    tiposDocumentos = [
+        ('T.I.', 'Tarjeta de Identidad'),
+        ('C.C.', 'Cedula de Ciudadanía'),
+        ('C.E.', 'Cedula de Extranjería')
+    ]
+    tipoDocumento = models.CharField(max_length=4, choices=tiposDocumentos ,default='T.I.')
+    documento = models.CharField(max_length=10, default=0)
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     edad = models.IntegerField()
-    grado = models.ForeignKey(Grados, on_delete=models.CASCADE, related_name="estudiantes")
     contrasena = models.CharField(max_length=100)
     faltasTipo1 = models.IntegerField(default=0)
     faltasTipo2 = models.IntegerField(default=0)
     faltasTipo3 = models.IntegerField(default=0)
+    idGrado = models.ForeignKey(Grado, on_delete=models.CASCADE, null=True, blank=True)
 
-    class Meta:
-        db_table = 'Estudiantes'
+    def __str__(self):
+        return f"{self.idEstudiante}, {self.apellido} {self.nombre}, Identificación: {self.tipoDocumento} {self.documento}"
 
-class Acudientes(models.Model):
+
+
+
+class Acudiente(models.Model):
     idAcudiente = models.AutoField(primary_key=True)
+    tiposDocumentos = [
+        ('T.I.', 'Tarjeta de Identidad'),
+        ('C.C.', 'Cedula de Ciudadanía'),
+        ('C.E.', 'Cedula de Extranjería')
+    ]
+    tipoDocumento = models.CharField(max_length=4, choices=tiposDocumentos ,default='C.C.')
+    documento = models.CharField(max_length=10, default=0)
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     telefono = models.CharField(max_length=10)  # Mejor CharField para conservar ceros iniciales
     correo = models.EmailField(max_length=100)  # EmailField valida automáticamente correos
     contrasena = models.CharField(max_length=100)
-    idEstudiante = models.ForeignKey(Estudiantes, on_delete=models.CASCADE, related_name="acudientes")
+    idEstudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE, related_name="acudientes")
 
-    class Meta:
-        db_table = 'Acudientes'
+    def __str__(self):
+        return f"Estudiante: {self.idEstudiante}, Apellido: {self.apellido}, Nombre: {self.nombre}"
 
 class Administrativos(models.Model):
     idAdministrativo = models.AutoField(primary_key=True)
@@ -46,36 +62,24 @@ class Administrativos(models.Model):
     correo = models.EmailField(max_length=100)
     contrasena = models.CharField(max_length=100)
 
-    class Meta:
-        db_table = 'Administrativos'
-
 class Faltas(models.Model):
     idFalta = models.AutoField(primary_key=True)
     tipoFalta = models.IntegerField()
     descripcion = models.TextField(max_length=1000)
 
-    class Meta:
-        db_table = 'Faltas'
-
-class Observaciones(models.Model):
+class Observacion(models.Model):
     idObservacion = models.AutoField(primary_key=True)
     idFalta = models.ForeignKey(Faltas, on_delete=models.CASCADE, related_name="observaciones")
-    idEstudiante = models.ForeignKey(Estudiantes, on_delete=models.CASCADE, related_name="observaciones")
+    idEstudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE, related_name="observaciones")
     idAdministrativo = models.ForeignKey(Administrativos, on_delete=models.CASCADE, related_name="observaciones")
-    idGrado = models.ForeignKey(Grados, on_delete=models.CASCADE, related_name="observaciones")
+    idGrado = models.ForeignKey(Grado, on_delete=models.CASCADE, related_name="observaciones")
     fecha = models.DateField()
     hora = models.TimeField()
     comentario = models.TextField(max_length=1000)
-
-    class Meta:
-        db_table = 'Observaciones'
 
 class Citaciones(models.Model):
     idCitacion = models.AutoField(primary_key=True)
     fecha = models.DateField()
     hora = models.TimeField()
-    idAcudiente = models.ForeignKey(Acudientes, on_delete=models.CASCADE, related_name="citaciones")
-    idEstudiante = models.ForeignKey(Estudiantes, on_delete=models.CASCADE, related_name="citaciones")
-
-    class Meta:
-        db_table = 'Citaciones'
+    idAcudiente = models.ForeignKey(Acudiente, on_delete=models.CASCADE, related_name="citaciones")
+    idEstudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE, related_name="citaciones")
