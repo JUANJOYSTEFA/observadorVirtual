d6dd489bd300a9cfa02fd42f966265d367e8afde
Juan José <juanjoseromerogomez0@gmail.com>
2025-03-09 22:42:21 -0500

    Agrego la lista de colegios

--- modulos/Observador/views.py
+++ modulos/Observador/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import *  # Importa el modelo
 
-# Create your views here.
+def listaColegios(request):
+    colegios = Colegio.objects.all()  # Obtiene todos los registros
+    return render(request, 'listaColegios.html', {'colegios': colegios})
