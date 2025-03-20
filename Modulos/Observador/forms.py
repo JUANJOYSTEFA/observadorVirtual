03bcd0d03fc66d101c6101e71b52fbe52902f7db
Juan José <juanjoseromerogomez0@gmail.com>
2025-03-19 23:38:03 -0500

    Agrego el crud de Grado y empiezo con el de acudientes

--- Modulos/Observador/forms.py
+++ Modulos/Observador/forms.py
@@ -1,9 +1,58 @@
 from django import forms
-from .models import Colegio
+from .models import *
 
 
 class ColegioForm(forms.ModelForm):
 
 	class Meta:
 		model = Colegio
+		fields = "__all__"
+
+
+class GradoForm(forms.ModelForm):
+
+	class Meta:
+		model = Grado
+		fields = "__all__"
+
+
+class EstudianteForm(forms.ModelForm):
+
+	class Meta:
+		model = Estudiante
+		fields = "__all__"
+
+
+class AcudienteForm(forms.ModelForm):
+
+	class Meta:
+		model = Acudiente
+		fields = "__all__"
+
+
+class AdministrativosForm(forms.ModelForm):
+
+	class Meta:
+		model = Administrativos
+		fields = "__all__"
+
+
+class FaltasForm(forms.ModelForm):
+
+	class Meta:
+		model = Faltas
+		fields = "__all__"
+
+
+class ObservacionForm(forms.ModelForm):
+
+	class Meta:
+		model = Observacion
+		fields = "__all__"
+
+
+class CitacionesForm(forms.ModelForm):
+
+	class Meta:
+		model = Citaciones
 		fields = "__all__"
--- observadorVirtual/settings.py
+++ observadorVirtual/settings.py
@@ -123,9 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'  # Si aún no lo tienes
+STATIC_URL = '/static/'  # Si aún no lo tienes
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    # 🔹 Agrega la carpeta global de estáticos
+    os.path.join(BASE_DIR, 'observadorVirtual/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
